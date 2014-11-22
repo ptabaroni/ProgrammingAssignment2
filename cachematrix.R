76ef9bd8337ef1a0f7cbcc155774365d04799ecf
ptabaroni <tabaroni+github@gmail.com>
2014-11-22 15:27:41 +0100

    Final version w/ comments and indentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,48 +1,48 @@
 ##constructor Than will define one instance of the cacheMatrix Obj
 ##Same as Vector example so far
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  get <- function() x
-  setcm <- function(inverse) i <<- inverse
-  getcm <- function() i
-  list(set = set, get = get,
-       setcm = setcm,
-       getcm = getcm) 
-  
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setcm <- function(inverse) i <<- inverse
+        getcm <- function() i
+        list(set = set, get = get,
+             setcm = setcm,
+             getcm = getcm) 
+        
 }
 
-##Here as well it follows the same example function
-##...i feel a little dirty but as long as it works...
+##Still not very different from the example, 
+##I've added a few status messages while solving the matrix for the first time 
 cacheSolve <- function(x, ...) {
-  i <- x$getcm()
-  if(!is.null(i)) {
-    message("getting cached data")
-    return(i)
-  }
-  data <- x$get()
-  message("calculating the inverse matrix of:")
-  print(data)
-  i <- solve(data)
-  x$setcm(i)
-  message("caching the result")
-  i
+        i <- x$getcm()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        message("calculating the inverse matrix of:")
+        print(data)
+        i <- solve(data)
+        x$setcm(i)
+        message("caching the result")
+        i
 }
 
-##check if everything is fine with a matrix multiplication between the original and its cached inverse
+##check if the cached inverse is correct by multiplicating it with the original
+##output is rounded to show only 1s and 0s
 checkCache  <- function(x, ...) {
-  o <- x$get()
-  i <- x$getcm()
-  if(is.null(i)){
-    stop("Inverse matrix not yet cached, run cacheSolve first")
-  }
-  else
-  {
-    ##rounding the proof matrix to show Ones and Zeroes only
-    elegant_proof <- round(o %*% i)
-    elegant_proof
-  }
+        o <- x$get()
+        i <- x$getcm()
+        if(is.null(i)){
+                stop("Inverse matrix not yet cached, run cacheSolve first")
+        }
+        else
+        {
+                elegant_proof <- round(o %*% i)
+                elegant_proof
+        }
 }
