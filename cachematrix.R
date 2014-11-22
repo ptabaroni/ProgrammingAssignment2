a3d4504eb297b64cf6e51442c744a182328c2ffe
ptabaroni <tabaroni+github@gmail.com>
2014-11-22 14:53:35 +0100

    It works! Just need some tests on more random matrixes

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,30 @@
-## Write a short comment describing this function
-
+##constructor Than will define one instance of the cacheMatrix Obj
+##Same as Vector example so far
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setcm <- function(inverse) i <<- inverse
+  getcm <- function() i
+  list(set = set, get = get,
+       setcm = setcm,
+       getcm = getcm) 
+  
 }
 
-
-## Write a short comment describing this function
-
+##Here as well it follows the same example function
+##...i feel a little dirty but as long as it works...
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getcm()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setcm(i)
+  i
 }
